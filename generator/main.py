dfd062063b98b1eee7a193e843e4c2a100ced032
Bradley Bellairs <bwbellairs@outlook.com>
2022-09-11 21:33:13 +0100

    Write projects.json
    Signed-off-by: Bradley Bellairs <bwbellairs@outlook.com>

--- generator/main.py
+++ generator/main.py
@@ -1,5 +1,30 @@
 from aitextgen import aitextgen
+import re
+import json
 
 ai = aitextgen(model_folder="../site", )
 
-projects = ai.generate(n=100, return_as_list=True)
+def mapProject(projectString):
+    values = projectString.split(" | ")
+    return {
+            "title": values[3],
+            "tagline": values[4].rstrip(),
+            "winner": values[0] == "True",
+            "likes": values[1],
+            "comments": values[2]
+    }
+
+def createBatch():
+    batch = []
+    while len(batch) < 100:
+        generated = ai.generate(n = 10, return_as_list=True)
+        generated = list(filter(lambda x: re.search(r"(True|False) \| \d+ \| \d+ \| .*? \| .*", x) != None, generated))
+        generated = list(map(mapProject, generated))
+        batch += generated
+    
+    return batch
+
+batch = createBatch()
+with open("./projects.json", "w") as batchFile:
+    json.dump(batch, batchFile)
+    batchFile.close()
