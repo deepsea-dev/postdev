1ea704940582b00900b03b8e33012760900103b4
Bradley Bellairs <bwbellairs@outlook.com>
2022-09-11 18:48:54 +0100

    Generate example projects in backend
    Signed-off-by: Bradley Bellairs <bwbellairs@outlook.com>

--- site/main.py
+++ site/main.py
@@ -1,24 +1,31 @@
 from flask import Flask, render_template
+import re
+from aitextgen import aitextgen
+ai = aitextgen(model_folder=".", to_gpu=False)
+
+def generateProjects():
+  generated = []
+  while len(generated) < 10:
+    line = ai.generate_one().rstrip()
+    print(line)
+    match = re.search(r"(True|False) \| \d+ \| \d+ \| .*? \| .*", line)
+    if match == None: continue
+
+    values = line.split("|")
+    generated.append({
+      "title": values[3],
+      "tagline": values[4],
+      "winner": values[0],
+      "likes": values[1],
+      "comments": values[2]
+    })
+
+  return generated
+
 
 app = Flask(__name__)
 
 @app.get('/')
 def index():
-  projects = [
-    {
-      "title": "Octocatch",
-      "tagline": "collect falling items at the bottom of the ocean as an octopus",
-      "winner": True,
-      "likes": "8453",
-      "comments": "7895"}
-  ] * 6
-
-  projects += [
-    {
-      "title": "Octocatch",
-      "tagline": "collect falling items at the bottom of the ocean as an octopus",
-      "winner": False,
-      "likes": "8453",
-      "comments": "7895"}
-  ] * 6
+  projects = generateProjects()
   return render_template('index.html', projects=projects)
